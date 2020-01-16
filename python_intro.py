1b54e12196277b0195a92710b2a28b72fb9a3dc0
Kleber Vitorio <teckleber@gmail.com>
2020-01-16 19:13:44 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+print('Hello, Django girls!')
+if 3 < 3:
+    print('Tres é menor que 3')
+elif 3 < 2:
+    print('Tres é menor que 2')
+else:
+    print('Tres é demais')
+
+def hi():
+    print('Criando minha função')
+
+hi()
